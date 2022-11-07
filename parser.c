3705fbede205eea274d7ba0c15bb56c8ccafe9ba
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-07 15:28:15 +0100

    analyse str format

---
+++ parser.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * parser - format controll
+ * @format: string format
+ * @ap: list of arguments
+ * Return: total len of argument with the total len of string
+ **/
+int parser(const char *format, va_list ap)
+{
+	int len, i, dm;
+
+	len = 0;
+	for (i = 0; format[i] != 0; i++)
+	{
+		if (format[i] == '%')
+		{
+			dm = percent_parser(format, ap, &i);
+			if (dm == -1)
+				return (-1);
+
+			len += dm;
+			continue;
+		}
+
+		putchar(format[i]);
+		len += 1;
+	}
+
+	return (len);
+}
