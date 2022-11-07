ca84ac9e22f854ff9607a3b42d483122e5e0c8d2
scorpion00100 <danbetheliryivuze@gmail.com>
2022-11-07 15:14:31 +0100

    analyse format

---
+++ percent_parser.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * percent_parser - controll percent format
+ * @format: the parser format
+ * @ap: list of arguments
+ * @i: iterator
+ * Return: the number of characters printed
+ **/
+int percent_parser(const char *format, va_list ap, int *i)
+{
+	int len, j;
+
+	to_format formats[] = {
+		{'c', print_char}, {'s', print_string},
+		{'d', print_integer}, {'i', print_integer},
+		{'\0', NULL}
+	};
+
+	*i = *i + 1;
+
+	if (format[*i] == '\0')
+		return (-1);
+
+	if (format[*i] == '%')
+	{
+		putchar('%');
+		return (1);
+	}
+
+	for (len = j = 0; formats[j].specifier; j++)
+	{
+		if (format[*i] == formats[j].specifier)
+		{
+			len = formats[j].f(ap);
+			return (len);
+		}
+
+	}
+
+	putchar('%');
+	putchar(format[*i]);
+
+	return (2);
+}
