9fd05a4006cc0976f31625682d54b964bfa57109
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-08 07:51:31 -0800

    print characters

---
+++ print_char.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_char - function that prints characters
+ * @md: variadic list
+ * Return: 1 if not error
+ */
+
+int print_char(va_list md)
+{
+	int dm;
+
+	dm = va_arg(md, int);
+
+	_putchar(dm);
+
+	return (1);
+
+}
