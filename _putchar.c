b23fcaf6c9452a25cb1b48b52494eab026d554c5
Nelyrma <marlyne.ihorimbere@gmail.com>
2022-11-09 00:45:05 -0800

    the_putchar_func_with_buffer

--- _putchar.c
+++ _putchar.c
@@ -1,37 +1,37 @@
 #include "main.h"
 
 /**
-* _putchar - write the character c to stdout
-* @c: the character to print
+* _putchar - write the character d to stdout
+* @d: the character to print
 *
 * Return: on success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
 
-int _putchar(char c)
+int _putchar(char d)
 {
-	return (buffer(c));
+	return (buffer(d));
 }
 
 /**
  * buffer - save a char in a buffer
- * @c: char
+ * @d: char
  *
  * Return: 1
  **/
-int buffer(char c)
+int buffer(char d)
 {
 	static char buffering[1024];
-	static int i;
+	static int m;
 
-	if (c == -1 || i == 1024)
+	if (d == -1 || m == 1024)
 	{
-		write(1, buffering, i);
-		i = 0;
+		write(1, buffering, m);
+		m = 0;
 	}
 
-	if (c != -1)
-		buffering[i++] = c;
+	if (d != -1)
+		buffering[m++] = d;
 
 	return (1);
 }
